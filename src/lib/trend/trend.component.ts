a2cb18f8ab20fbfb68c1700956773466eb5630dd
louis <louis@lemoncloud.io>
2019-07-17 13:01:17 +0900

    fix: fix animation issue

--- src/lib/trend/trend.component.ts
+++ src/lib/trend/trend.component.ts
@@ -113,11 +113,11 @@ import { normalizeDataset } from './trend.helpers';
       ]),
     ]),
     trigger('circleAnimation', [
-      state('inactive', style({ display: 'none' })),
+      state('inactive', style({ visibility: 'hidden', opacity: 0 })),
       transition('* => active', [
-        style({ display: 'none' }),
+        style({ visibility: 'hidden' }),
         animate('{{ autoDrawDuration }}ms',
-          keyframes([ style({ display: 'block' }),
+          keyframes([ style({ visibility: 'visible' }),
           ]),
         ),
       ]),
